5822ac63a6f1264aebccf082f35bc070e166e24f
Sohil2233 <tambolisohil04@gmail.com>
2024-02-08 09:30:03 +0530

    BASICS OF PYTHON

---
+++ 1day.py
@@ -0,0 +1,36 @@
+# Write your code below this line 👇
+# print("Namaste Duniya")
+
+# new line
+print("Namaste Duniya\nNamaste Duniya")
+
+# string concatiation
+# print("soul"+"music")
+
+# print("soul"+"  "+"music")
+
+# print("soul"+"music")
+
+
+# input function
+# input("what is rap name?")
+
+# print("soul"+" "+input("what is rap name?"))
+
+# python varibales
+# name="soul"
+# print(name)
+# name="music"
+# print(name)
+
+# name=input("who are you")
+# length=len(name)
+# print(length)
+
+# variable naming
+# user_name="soul"  
+# print(user_name)
+# name1=23
+# print(name1)
+# name2=56
+# print(name2)
---
+++ 2day.py
@@ -0,0 +1,45 @@
+#Data Types
+#String
+# print("Namaste"[2])
+# print("123"+"456")
+# print(123+456)
+
+
+#Floating
+# 3.14159
+# Boolean
+# print('True')
+
+# num_char = len(input("What is your name?"))
+# print(type(num_char))
+# print("your name has " +str(num_char)+ " characters")
+
+# typecasting
+# print(70+float("100.5"))
+# print(str(70)+str(100))
+
+# operator
+# print(type(2+3))
+# print(type(2-3))
+# print(type(2*3))
+# print(type(2/3))
+# print(2**3)
+
+# print(3*3+3/3-3)
+
+# print(int(8/3))
+# print(round(8/3))#round off
+# print(round(8/3,2))#round off to 2 decimal places
+# print(8//3)#floor division not decimal
+# result = 4/2
+# result /= 2
+# print(result)
+
+
+# score = 0
+# height = 1.8
+# isWinning = True
+# #f-string
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+ 
