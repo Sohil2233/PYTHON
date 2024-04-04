be2babae8d14eb3e3a7134266475298adc66dd3d
Sohil2233 <tambolisohil04@gmail.com>
2024-04-04 14:01:58 +0530

    BASICS OF PYHTON

---
+++ 3day.py
@@ -0,0 +1,82 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# if height > 120:
+#   print("You can ride the rollercoaster!")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+# Comparison Operators
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+# Modulo Operation
+# 7 % 2
+# 2 + 2 + 2 + 1
+# 7 % 3
+# 3 + 3 + 1
+# Nested if/else
+# if condition:
+#   if another condition:
+#     do this
+#   else:
+#     do this
+# else:
+#   do this
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# if height>120:
+#   print("You can ride the rollercoaster!")
+#   age = int(input("What is your age? "))
+#   if age < 12:
+#     print("Please pay $5.")
+#   elif age <= 18:
+#     print("Please pay $7.")
+#   else:
+#     print("Please pay $12.")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+# Multiple if statements in succession
+# if condition1:
+#   do A
+# if condition2:
+#   do B
+# if condition3:
+#   do C
+# Logical Operators
+# A and B
+# C or D
+# not E
+# 
+
+
+# Love Calculator
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+# combined_string = name1 + name2
+# lower_case_string = combined_string.lower()
+# t = lower_case_string.count("t")
+# r = lower_case_string.count("r")
+# u = lower_case_string.count("u")
+# e = lower_case_string.count("e")
+# true = t + r + u + e
+# l = lower_case_string.count("l")
+# o = lower_case_string.count("o")
+# v = lower_case_string.count("v")
+# e = lower_case_string.count("e")
+# love = l + o + v + e
+# love_score = int(str(true) + str(love))
+# if (love_score < 10) or (love_score > 90):
+#   print(f"Your score is {love_score}, you go together like coke and mentos.")
+# elif (love_score >= 40) and (love_score <= 50):
+#   print(f"Your score is {love_score}, you are alright together.")
+# else:
+#   print(f"Your score is {love_score}.")
+
+
+
+
