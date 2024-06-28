dfebd68d23769ed20672b44aaf36b99367a45965
Sohil2233 <tambolisohil04@gmail.com>
2024-06-28 23:43:29 +0530

    .

--- college python/exception/Exp7a.py
+++
@@ -1,6 +0,0 @@
-def divide_by_zero():
-    result = 10 / 0
-try:
-    divide_by_zero()
-except ZeroDivisionError:
-    print("Error: Division by zero occurred. Abnormal termination.")
--- college python/file_manipulation/Exp6b.py
+++
@@ -1,4 +0,0 @@
-with open("output.txt", "a") as file:
-    file.write("hi python programming\n")
-
-print("Successfully appended 'hi python programming' to output.txt")
